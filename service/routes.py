3ec76ccaad6df72ceac404e3457e01f3fcc3c84a
SamuelSloboda <ss16249@nyu.edu>
2022-10-17 01:01:56 -0400

    Adding Customer

--- service/routes.py
+++ service/routes.py
@@ -18,8 +18,13 @@ from . import app
 @app.route("/")
 def index():
     """ Root URL response """
+    app.logger.info("Request for Root URL")
     return (
-        "Reminder: return some useful information in json format about the service here",
+         jsonify(
+            name="Customer Demo REST API Service",
+            version="1.0",
+            #paths=url_for("list_customers", _external=True),
+        ), 
         status.HTTP_200_OK,
     )
 
@@ -33,3 +38,41 @@ def init_db():
     """ Initializes the SQLAlchemy app """
     global app
     Customer.init_db(app)
+
+def check_content_type(content_type):
+    """Checks that the media type is correct"""
+    if "Content-Type" not in request.headers:
+        app.logger.error("No Content-Type specified.")
+        abort(
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            f"Content-Type must be {content_type}",
+        )
+
+    if request.headers["Content-Type"] == content_type:
+        return
+
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {content_type}",
+    )
+
+######################################################################
+# ADD A NEW CUSTOMER
+######################################################################
+@app.route("/customers", methods=["POST"])
+def create_customers():
+    """
+    Creates a Customer
+    This endpoint will create a Customer based the data in the body that is posted
+    """
+    app.logger.info("Request to create a customer")
+    check_content_type("application/json")
+    customer = Customer()
+    customer.deserialize(request.get_json())
+    customer.create()
+    message = customer.serialize()
+    #location_url = url_for("get_customers", customer_id=customer.id, _external=True)
+
+    app.logger.info("Customer with ID [%s] created.", customer.id)
+    return jsonify(message), status.HTTP_201_CREATED #, {"Location": location_url}
