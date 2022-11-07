b2e3cdb00552bae1105e91af67616880de19ce6c
kovidiament <kovidiament@gmail.com>
2022-11-07 17:26:02 +0000

    cli_commands added to service and to test

--- service/__init__.py
+++ service/__init__.py
@@ -17,7 +17,7 @@ app.config.from_object(config)
 # pylint: disable=wrong-import-position, wrong-import-order
 from service import routes         # noqa: E402, E261
 # pylint: disable=wrong-import-position
-from .common import error_handlers  # noqa: F401 E402
+from .common import error_handlers, cli_commands  # noqa: F401 E402
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
